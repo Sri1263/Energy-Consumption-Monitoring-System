a48c2a40beaab67c46b70d33f253409424e83eb5
TRINoo7 <balakrishnangirivigneshtrin@gmail.com>
2023-10-05 13:32:59 +0530

    Updates App.js ( contact )

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './Components/Home';
 import Login from './Components/Login';
 import Dashboard from './Components/Dashboard';
+import Contact from './Components/Contact'
 
 function App() {
   return (
